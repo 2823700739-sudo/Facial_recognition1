d345e74d6e5a59de355bc26342c8c99490408b15
Crystal <16614141+crystal70777@user.noreply.gitee.com>
2026-04-01 15:36:24 +0800

    feat: 添加Qt项目文件和OpenCV库支持
    主要变更包括:
    - 添加Qt项目文件project.pro
    - 添加OpenCV库头文件和库文件路径
    - 实现登录界面UI和相关功能
    - 添加OpenCV相关依赖库
    - 更新构建配置

---
+++ build-project-Desktop_Qt_5_14_2_MinGW_64_bit-Release/ui_login.h
@@ -0,0 +1,61 @@
+/********************************************************************************
+** Form generated from reading UI file 'login.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_LOGIN_H
+#define UI_LOGIN_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_login
+{
+public:
+    QWidget *centralwidget;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *login)
+    {
+        if (login->objectName().isEmpty())
+            login->setObjectName(QString::fromUtf8("login"));
+        login->resize(800, 600);
+        centralwidget = new QWidget(login);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        login->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(login);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        login->setMenuBar(menubar);
+        statusbar = new QStatusBar(login);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        login->setStatusBar(statusbar);
+
+        retranslateUi(login);
+
+        QMetaObject::connectSlotsByName(login);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *login)
+    {
+        login->setWindowTitle(QCoreApplication::translate("login", "login", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class login: public Ui_login {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_LOGIN_H
